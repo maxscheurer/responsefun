046bc2b2647b53fdbb128ab09038e584e4819a86
apapapostolou <Papapostolou@stud.uni-heidelberg.de>
2022-07-07 13:07:50 +0200

    evaluate gamma extra terms with adc/isr

--- responsefun/gamma_extra_terms.py
+++ responsefun/gamma_extra_terms.py
@@ -2,6 +2,10 @@ import numpy as np
 from itertools import permutations, product
 import adcc
 from pyscf import gto, scf
+from adcc.workflow import construct_adcmatrix
+from adcc.adc_pp import modified_transition_moments
+from respondo.misc import select_property_method
+from respondo.solve_response import solve_response
 
 
 def sos_antonia(state, omega_1, omega_2, omega_3):
@@ -40,6 +44,39 @@ def sos_panor(state, omega_1, omega_2, omega_3):
     return sos
 
 
+def sos_panor_single(state, omega_1, omega_2, omega_3):
+    omega_o = omega_1 + omega_2 + omega_3
+    tf = state.excitation_energy_uncorrected
+    sos = np.zeros((3, 3, 3, 3))
+    components = list(product([0, 1, 2], repeat=4))
+    for n, dip_n in enumerate(state.transition_dipole_moment):
+        for m, dip_m in enumerate(state.transition_dipole_moment):
+            for c in components:
+                A, B, C, D = c
+                sos[c] += dip_n[A]*dip_n[B]*dip_m[C]*dip_m[D] / (
+                        (tf[n]-omega_o) * (tf[m]-omega_3) * (tf[m]+omega_2)
+                )
+    return sos
+
+
+def isr_panor_single(state, omega_1, omega_2, omega_3):
+    omega_o = omega_1 + omega_2 + omega_3
+    matrix = construct_adcmatrix(state.matrix)
+    property_method = select_property_method(matrix)
+    mp = matrix.ground_state
+    dips = state.reference_state.operators.electric_dipole
+    mtms = modified_transition_moments(property_method, mp, dips)
+    response1 = [solve_response(matrix, mtm, omega_o, gamma=0.0) for mtm in mtms]
+    response2 = [solve_response(matrix, mtm, omega_3, gamma=0.0) for mtm in mtms]
+    response3 = [solve_response(matrix, mtm, -omega_2, gamma=0.0) for mtm in mtms]
+    isr = np.zeros((3, 3, 3, 3))
+    components = list(product([0, 1, 2], repeat=4))
+    for c in components:
+        A, B, C, D = c
+        isr[c] = (response1[A] @ mtms[B]) * (response2[C] @ response3[D])
+    return isr
+
+
 mol = gto.M(
     atom="""
     O 0 0 0
@@ -60,8 +97,13 @@ matrix = adcc.AdcMatrix("adc2", refstate)
 state = adcc.adc2(scfres, n_singlets=65)
 #print(state.describe())
 
-panor_terms = sos_panor(state, 0.0, 0.0, 0.0)
-#antonia_terms = sos_antonia(state, 0.5, 0.3, 0.0)
-print(panor_terms)
+#panor_terms = sos_panor(state, 0.5, 0.4, 0.3)
+#antonia_terms = sos_antonia(state, 0.5, 0.4, 0.3)
+#print(panor_terms)
 #print(antonia_terms)
 #np.testing.assert_allclose(panor_terms, antonia_terms, atol=1e-7)
+sos = sos_panor_single(state, 0.0, 0.0, 0.0)
+isr = isr_panor_single(state, 0.0, 0.0, 0.0)
+print(sos)
+print(isr)
+np.testing.assert_allclose(sos, isr, atol=1e-7)
