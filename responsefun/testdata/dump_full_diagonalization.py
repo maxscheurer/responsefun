f009703faa3bf1e4568bbcaa362a5091d4d25be8
apapapostolou <Papapostolou@stud.uni-heidelberg.de>
2023-05-05 17:32:32 +0200

    forgotten file

--- responsefun/testdata/dump_full_diagonalization.py
+++ responsefun/testdata/dump_full_diagonalization.py
@@ -63,8 +63,6 @@ def main():
                 continue
             exci[key] = d
 
-        # TODO: remove line once PR #158 of adcc has been merged
-        exci["transition_magnetic_dipole_moment"] = transition_moments(state, mdips)
         exci["transition_dipole_moment_s2s"] = s2s_tdms
         exci["transition_magnetic_moment_s2s"] = s2s_tdms_mag
         exci.attrs["kind"] = state.kind
