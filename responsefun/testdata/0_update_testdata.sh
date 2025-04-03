f9f9bdb1fea2b9baf64b380a1032b598651ec16a
Friederike Schneider <friederike.schneider@iwr.uni-heidelberg.de>
2025-04-03 10:27:57 +0200

    store testdata in repo

--- responsefun/testdata/0_update_testdata.sh
+++ responsefun/testdata/0_update_testdata.sh
@@ -2,7 +2,6 @@
 
 # taken from respondo
 
-SOURCE="https://wwwagdreuw.iwr.uni-heidelberg.de/responsefun_test_data/0.1.0"
 DATAFILES=(
 	data_0.1.0.tar.gz
 )
@@ -13,18 +12,6 @@ DATAFILES=(
 THISDIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$THISDIR"
 echo "Updating testdata ... please wait."
-
-download() {
-	if which wget &> /dev/null; then
-	    echo $@
-		wget -w 1 -qN --show-progress --no-check-certificate $@
-	else
-		echo "wget not installed" >&2
-		exit 1
-	fi
-}
-
-download $(for file in ${DATAFILES[@]}; do echo $SOURCE/$file; done)
 
 if which sha256sum &> /dev/null; then
 	sha256sum --ignore-missing -c SHA256SUMS || exit 1
