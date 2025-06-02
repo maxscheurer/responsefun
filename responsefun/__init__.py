e75ce4e47e75a0f3147845227a7a4925890fdf49
Friederike Schneider <110526409+frieschneider@users.noreply.github.com>
2025-06-02 12:22:59 +0200

    Bump version: 0.2.0 → 0.3.0 (#14)

--- responsefun/__init__.py
+++ responsefun/__init__.py
@@ -5,7 +5,7 @@ from .evaluate_property import (
     evaluate_property_sos_fast,
 )
 from .SumOverStates import TransitionMoment
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["__version__", "evaluate_property_isr", "evaluate_property_sos",
            "evaluate_property_sos_fast", "TransitionMoment"]
